4adcaee3e488ace59284923071033381ff4c0dd8
B Marinov <dev@borismarinov.de>
2026-05-07 20:58:01 +0200

    Move retry func to separate file

--- internal/incus/incus_client.go
+++ internal/incus/incus_client.go
@@ -190,31 +190,3 @@ func (c *Client) reconnect(current *conn) error {
 	c.srv.Store(&conn{srv: srv})
 	return nil
 }
-
-func retry[T any](ctx context.Context,
-	p retryPolicy,
-	op func() (result T, err error),
-	shouldRetry func(error) bool,
-) (T, error) {
-	var result T
-	var err error
-	for range p.attempts {
-		select {
-		case <-ctx.Done():
-			return result, ctx.Err()
-		default:
-			result, err = op()
-			if err != nil && shouldRetry(err) {
-				select {
-				case <-ctx.Done():
-					return result, ctx.Err()
-				case <-time.After(p.delay):
-				}
-			} else {
-				return result, err
-			}
-		}
-	}
-
-	return result, err
-}
--- internal/incus/retry.go
+++ internal/incus/retry.go
@@ -1,10 +1,42 @@
 package incus
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type retryPolicy struct {
 	// number of retries
 	attempts int
 	// between attempts
 	delay time.Duration
+}
+
+func retry[T any](
+	ctx context.Context,
+	p retryPolicy,
+	op func() (result T, err error),
+	shouldRetry func(error) bool,
+) (T, error) {
+	var result T
+	var err error
+	for range p.attempts {
+		select {
+		case <-ctx.Done():
+			return result, ctx.Err()
+		default:
+			result, err = op()
+			if err != nil && shouldRetry(err) {
+				select {
+				case <-ctx.Done():
+					return result, ctx.Err()
+				case <-time.After(p.delay):
+				}
+			} else {
+				return result, err
+			}
+		}
+	}
+
+	return result, err
 }
